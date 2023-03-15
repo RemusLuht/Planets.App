8bb90bfcc2ef4dd5054cf3557eea107f267c2d7c
opilane <opilane@HP-CZC0359NN9>
2023-03-15 15:58:39 +0200

    Add project files.

---
+++ MauiProgram.cs
@@ -0,0 +1,19 @@
+﻿namespace Planets;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("", "RegularFont");
+				fonts.AddFont("", "MediumFont");
+				fonts.AddFont("", "BoldFont");
+            });
+
+		return builder.Build();
+	}
+}
