9d4756f454ee74caff1b1dd48d2e3bc43d013128
Klesman123 <marcos.klesman@gmail.com>
2023-03-22 16:40:38 +0200

    a

--- Views/PlanetsPage.xaml.cs
+++ Views/PlanetsPage.xaml.cs
@@ -2,8 +2,21 @@ namespace Views;
 
 public partial class PlanetsPage : ContentPage
 {
-	public PlanetsPage()
+	private const uint AnimationDuration = 800u;
+
+    public PlanetsPage()
 	{
 		InitializeComponent();
 	}
+	async void GridArea_Tapped(System.Object sender, System.EventArgs e)
+	{
+		await CloseMenu();
+	}
+
+	private async Task CloseMenu()
+	{
+		_ = MainContentGrid.FadeTo(1, AnimationDuration);
+        _ = MainContentGrid.ScaleTo(1, AnimationDuration);
+        await MainContentGrid.TranslateTo(0, 0, AnimationDuration, Easing.CubicIn);
+    }
 }
